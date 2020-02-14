751d32e182a13d83fc748453767cba1e1f08f665
divyareddyvanakuri <divyavanakuri48@gmail.com>
2020-02-14 15:16:52 +0530

    [Divya] Refactor: refactor the feet and inch object instance comparison's by passing different values

--- heightconverter/main.py
+++ heightconverter/main.py
@@ -1,13 +1,22 @@
-from feetclass import Feet
-from inchclass import inch
-first_one_feet=Feet(1)
-second_on_feet=Feet(2)
-try:
-    assert first_one_feet == second_on_feet
-    print("successful")
-except AssertionError as e:
-    print(e)
+from feetclass import FeetUnit
+from inchclass import InchUnit
 
+#1ft = 1ft
+first_foot_object = FeetUnit(1)
+second_foot_object = FeetUnit(1)
+print("Is 1ft = 1ft:",first_foot_object ==second_foot_object)
 
+#1ft = 2ft
+one_feet_object = FeetUnit(1)
+two_feet_object = FeetUnit(2)
+print("Is 1ft = 2ft:",one_feet_object == two_feet_object)
 
+#1inch = 1inch
+first_inch_object=InchUnit(1)
+second_inch_object=InchUnit(1)
+print("Is 1inch = 1inch:",first_inch_object == second_inch_object)
 
+#1inch = 2inch
+one_inch_object = InchUnit(1)
+two_inch_object = InchUnit(2)
+print("Is 1inch = 2inch:",one_feet_object == two_feet_object)
