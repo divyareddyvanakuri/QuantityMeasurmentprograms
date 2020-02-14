f618d9fb15695c11b781663487c5bb0252dc013f
divyareddyvanakuri <divyavanakuri48@gmail.com>
2020-02-14 16:52:34 +0530

    [Divya] Refactor: Added the one more case for yard class object instace comparison

--- heightconverter/main.py
+++ heightconverter/main.py
@@ -1,5 +1,6 @@
 from feetclass import FeetUnit
 from inchclass import InchUnit
+from yardclass import YardUnit
 
 #1ft = 1ft
 first_foot_object = FeetUnit(1)
@@ -25,3 +26,8 @@ print("Is 1inch = 2inch:",one_inch_object == two_inch_object)
 first_yard_object=InchUnit(1)
 second_yard_object=InchUnit(1)
 print("Is 1yard = 1yard:",first_yard_object == second_yard_object)
+
+#1yard != 2yard
+one_yard_object = YardUnit(1)
+two_yard_object = YardUnit(2)
+print("Is 1yard = 2yard:",one_yard_object == two_yard_object)
