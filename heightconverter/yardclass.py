b6ea2248930618dbb22ae1c97b19fd89fb9e69fd
divyareddyvanakuri <divyavanakuri48@gmail.com>
2020-02-14 16:50:54 +0530

    [Divya] Refactor: Added the __eq__ or dunder method to yard class

--- heightconverter/yardclass.py
+++ heightconverter/yardclass.py
@@ -2,4 +2,10 @@ class YardUnit:
     def __init__(self, value):
         self.value = value
 
-    
+    def __eq__(self, other):
+        if isinstance(other, YardUnit):
+            if other.value == self.value:
+                return True
+
+        return False
+
