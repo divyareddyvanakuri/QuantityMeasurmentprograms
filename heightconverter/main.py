39363b02f8b145dc51ed4360dbad58529f28f592
divyareddyvanakuri <divyavanakuri48@gmail.com>
2020-02-14 16:20:04 +0530

    [Divya]Refactor: Added the funtional logic of the yard unit instance objects comparison

--- heightconverter/main.py
+++ heightconverter/main.py
@@ -6,7 +6,7 @@ first_foot_object = FeetUnit(1)
 second_foot_object = FeetUnit(1)
 print("Is 1ft = 1ft:",first_foot_object ==second_foot_object)
 
-#1ft = 2ft
+#1ft != 2ft
 one_feet_object = FeetUnit(1)
 two_feet_object = FeetUnit(2)
 print("Is 1ft = 2ft:",one_feet_object == two_feet_object)
@@ -16,7 +16,12 @@ first_inch_object=InchUnit(1)
 second_inch_object=InchUnit(1)
 print("Is 1inch = 1inch:",first_inch_object == second_inch_object)
 
-#1inch = 2inch
+#1inch != 2inch
 one_inch_object = InchUnit(1)
 two_inch_object = InchUnit(2)
-print("Is 1inch = 2inch:",one_feet_object == two_feet_object)
+print("Is 1inch = 2inch:",one_inch_object == two_inch_object)
+
+#1yard = 1yard
+first_yard_object=InchUnit(1)
+second_yard_object=InchUnit(1)
+print("Is 1yard = 1yard:",first_yard_object == second_yard_object)
