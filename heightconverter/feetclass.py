3481ffc0817d727313f0fbecd0769626d1f32cc0
divyareddyvanakuri <divyavanakuri48@gmail.com>
2020-02-14 15:24:54 +0530

    [Divya] Refactor: Added the conditional statement for to check object instance values are equal or not

--- heightconverter/feetclass.py
+++ heightconverter/feetclass.py
@@ -1,8 +1,10 @@
-class Feet:
-    def __init__(self,value):
-        self.value=value
-    def __eq__(self,other):
-        if  isinstance(other,Feet):
-            return True
-        else:
-            return False
+class FeetUnit:
+    def __init__(self, value):
+        self.value = value
+
+    def __eq__(self, other):
+        if isinstance(other, FeetUnit):
+            if other.value == self.value:
+                return True
+
+        return False
