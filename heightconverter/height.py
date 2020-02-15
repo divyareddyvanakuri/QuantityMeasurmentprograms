65b953afa7ffb3fac6dd3ad9fff136ee4ad10fe9
divyareddyvanakuri <divyavanakuri48@gmail.com>
2020-02-15 18:46:43 +0530

    [Divya].Refactor: removed the inherited class from parent class

--- heightconverter/height.py
+++ heightconverter/height.py
@@ -1,65 +1,56 @@
 from abc import ABC, abstractmethod
 
 
-class Height(ABC):
-    def __init__(self, value,conversion_factor):
+class Length(ABC):
+    def __init__(self, value, conversion_factor):
         self.value = value
         self.conversion_factor = conversion_factor
 
-    
     @abstractmethod
     def comparisons_to_base(self):
         pass
 
-class FeetUnit(Height):
-    def __init__(self):
-        super().__init__(0.3048)
 
-class InchUnit(Height):
-    def __init__(self):
-        super().__init__(0.0254)
-   
-
-class YardUnit(Height):
-    def __init__(self):
-        super().__init__(0.9144)
-
-class meterUnit(Height):
+class InchUnit(Length):
     def __init__(self):
         super().__init__(1)
 
 
-class Quantity(Height):
+class FeetUnit(Length):
+    def __init__(self):
+        super().__init__(1)
+
+
+class Quantity(Length):
     def __init__(self, value, conversion_factor):
-        # super().__init__(value, conversion_factor)
-        self.comparisons_to_base()
+        super().__init__(value, conversion_factor)
 
     def __eq__(self, other):
-        print(self.conversion_factor)
-        print(other.conversion_factor)
-        print(other.value)
-        print(self.value)
-    
-        if other.value<self.value:
-            other.value=other.value*12
-        elif other.value>self.value:
-             other.value=int(other.value/12)
-        else:
-            return False
-        print(other.value)
-        print(self.value)
         if self.value == other.value:
-            if isinstance(other,self.__class__):
-                return True
+            return True
         return False
         print(self.conversion_factor)
         print(other.conversion_factor)
+
     def comparisons_to_base(self):
         pass
 
-one_Feet = Quantity(1,FeetUnit)
-twelve_inch = Quantity(12,InchUnit)
-print(one_Feet == twelve_inch)
-one_Feet = Quantity(1,FeetUnit)
-twelve_inch = Quantity(1,InchUnit)
-print(one_Feet == twelve_inch)
+
+# 1 inch = 1 inch
+first_inch_object = Quantity(1, InchUnit)
+second_inch_object = Quantity(1, InchUnit)
+print("Is 1inch = 1inch: ", first_inch_object == second_inch_object)
+# 1 inch != 2 inch
+three_inch_object = Quantity(1, InchUnit)
+four_inch_object = Quantity(2, InchUnit)
+print("Is 1inch = 2inch: ", three_inch_object == four_inch_object)
+
+# 1 ft = 1 ft
+first_feet_object = Quantity(1, FeetUnit)
+second_feet_object = Quantity(1, FeetUnit)
+print("Is 1ft = 1ft: ", first_feet_object == second_feet_object)
+
+# 1 ft = 2 ft
+three_feet_object = Quantity(1, FeetUnit)
+four_feet_object = Quantity(2, FeetUnit)
+print("Is 1ft = 2ft: ", three_feet_object == four_feet_object)
