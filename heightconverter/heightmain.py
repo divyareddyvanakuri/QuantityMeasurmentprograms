c69200c313752c9b232815dd19cb23d25a2cc760
divyareddyvanakuri <divyavanakuri48@gmail.com>
2020-02-17 14:40:44 +0530

    [Divya].add:

---
+++ heightconverter/heightmain.py
@@ -0,0 +1,46 @@
+from height import InchUnit
+from height import FeetUnit
+from height import Length
+class Quantity:
+    def __init__(self,value, length):
+       
+        self.value=value
+        print(self.value)
+        self.unit = length
+        print(self.unit)
+        print(length.conversion_factor)
+
+    def comparisons_to_base(self):
+        pass
+
+    # def __str__(self):
+    #     return self.conversion_factor
+        
+    def __eq__(self, other):
+        if self.value  == other.value:
+            return True
+        return False
+
+# 1 inch = 1 inch
+first_inch_object = Quantity(1, InchUnit)
+second_inch_object = Quantity(1, InchUnit)
+print("Is 1inch = 1inch: ", first_inch_object == second_inch_object)
+# 1 inch != 2 inch
+three_inch_object = Quantity(1, InchUnit)
+four_inch_object = Quantity(2, InchUnit)
+print("Is 1inch = 2inch: ", three_inch_object == four_inch_object)
+
+# 1 ft = 1 ft
+first_feet_object = Quantity(1, FeetUnit)
+second_feet_object = Quantity(1, FeetUnit)
+print("Is 1ft = 1ft: ", first_feet_object == second_feet_object)
+
+# 1 ft = 2 ft
+three_feet_object = Quantity(1, FeetUnit)
+four_feet_object = Quantity(2, FeetUnit)
+print("Is 1ft = 2ft: ", three_feet_object == four_feet_object)
+
+one_feet = Quantity(1, FeetUnit)
+twelve_inches = Quantity(12, InchUnit)
+one_feet.comparisons_to_base()
+print("Is 1ft = 12inches: ", one_feet == twelve_inches)
