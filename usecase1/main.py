3863ec507bb99f0990e8425ed19aad8ed17b5500
divyareddyvanakuri <divyavanakuri48@gmail.com>
2020-02-17 18:11:16 +0530

    [Divya] refactor: Refactor the indentation and added the onemore test case for is 1 ft== 1inch

--- usecase1/main.py
+++ usecase1/main.py
@@ -19,23 +19,29 @@ class Quantity:
 # 1 inch = 1 inch
 first_inch_object = Quantity(1, InchUnit())
 second_inch_object = Quantity(1, InchUnit())
-print("Is 1inch = 1inch: ", first_inch_object == second_inch_object)
+print("Is 1inch = 1inch:", first_inch_object == second_inch_object)
 # 1 inch != 2 inch
 three_inch_object = Quantity(1, InchUnit())
 four_inch_object = Quantity(2, InchUnit())
-print("Is 1inch = 2inch: ", three_inch_object == four_inch_object)
+print("Is 1inch = 2inch:", three_inch_object == four_inch_object)
 
 # 1 ft = 1 ft
 first_feet_object = Quantity(1, FeetUnit())
 second_feet_object = Quantity(1, FeetUnit())
-print("Is 1ft = 1ft: ", first_feet_object == second_feet_object)
+print("Is 1ft = 1ft:", first_feet_object == second_feet_object)
 
 # 1 ft = 2 ft
 three_feet_object = Quantity(1, FeetUnit())
 four_feet_object = Quantity(2, FeetUnit())
-print("Is 1ft = 2ft: ", three_feet_object == four_feet_object)
+print("Is 1ft = 2ft:", three_feet_object == four_feet_object)
+
+#1 ft = 1 inch
+one_feet = Quantity(1, FeetUnit())
+one_inch = Quantity(1, InchUnit())
+print("Is 1ft = 1 inch:",one_feet==one_inch)
 
 # 1 ft = 12 inch
 one_feet = Quantity(1, FeetUnit())
 twelve_inches = Quantity(12, InchUnit())
-print("Is 1ft = 12inches: ", one_feet == twelve_inches)
+print("Is 1ft = 12inch:", one_feet == twelve_inches)
+
