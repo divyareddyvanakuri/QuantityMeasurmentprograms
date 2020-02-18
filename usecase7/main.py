249712d2379295c9204f58c1bac4d74a2069ed36
divyareddyvanakuri <divyavanakuri48@gmail.com>
2020-02-18 17:10:15 +0530

    [Divya]add: Implemented the composition cls for weights and created the usecases for compare and addition of two volumes in grams

---
+++ usecase7/main.py
@@ -0,0 +1,36 @@
+from weights import KgUnit, GramUnit, TonneUnit
+
+
+class Qunatity:
+    def __init__(self, value, unit):
+        self.value = value
+        self.unit = unit
+
+    def __eq__(self, other):
+        self.factor = int(self.unit.comparison_to_base(self.value))
+        other.factor = int(other.unit.comparison_to_base(other.value))
+        if self.factor == other.factor:
+            return True
+        return False
+
+    def __add__(self, other):
+        self.factor = int(self.unit.comparison_to_base(self.value))
+        other.factor = int(other.unit.comparison_to_base(other.value))
+        return self.factor+other.factor
+
+
+# 1 kg = 1000 grams
+one_kg_object = Qunatity(1, KgUnit())
+grams_object = Qunatity(1000, GramUnit())
+print("Is 1 kg = 1000 grams:", one_kg_object == grams_object)
+
+# 1 tonne = 1000 kg
+tonne_object = Qunatity(1, TonneUnit())
+kg_object = Qunatity(1000, KgUnit())
+print("Is 1 tonne = 1000 kg:", tonne_object == kg_object)
+
+# 1 tonne + 1000 grams
+tonne_object = Qunatity(1, TonneUnit())
+grams_object = Qunatity(1000, GramUnit())
+z = tonne_object+grams_object
+print("Add two weights 1 tonne and 1000 grams in kg:", z)
