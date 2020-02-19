1aa7b187b41ffbf9d417b91b97d6d9c074e3e24c
divyareddyvanakuri <divyavanakuri48@gmail.com>
2020-02-19 10:06:43 +0530

    [Divya]refactor: Added the comment lines in the file

--- usecase3/lenght.py
+++ usecase3/lenght.py
@@ -1,20 +1,19 @@
 from abc import ABC, abstractmethod
 
-
+#Abstract class for length
 class Length(ABC):
     def __init__(self, conversion_factor):
         self.conversion_factor = conversion_factor
 
-    
     def comparison_to_base(self, value):
         return value*self.conversion_factor
 
-
+#Derived class from the base class
 class InchUnit(Length):
     def __init__(self):
         super().__init__(5)
 
-
+#Derived class from the base class
 class CentiMeterUnit(Length):
     def __init__(self):
         super().__init__(2)
