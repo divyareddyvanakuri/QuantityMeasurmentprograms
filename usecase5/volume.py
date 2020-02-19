787b7d00e0e267ad787773655376298266a8d183
divyareddyvanakuri <divyavanakuri48@gmail.com>
2020-02-19 10:48:22 +0530

    [Divya]refactor: Added the comment lines to the code

--- usecase5/volume.py
+++ usecase5/volume.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 
 
+# Abstract class for Volume
 class Volume(ABC):
     def __init__(self, conversion_factor):
         self.conversion_factor = conversion_factor
@@ -9,16 +10,19 @@ class Volume(ABC):
         return value*self.conversion_factor
 
 
+#Derived class from the base class
 class GallonUnit(Volume):
     def __init__(self):
         super().__init__(3.78)
 
 
+#Derived class from the base class
 class LitreUnit(Volume):
     def __init__(self):
         super().__init__(1)
 
 
+#Derived class from the base class
 class MlUnit(Volume):
     def __init__(self):
         super().__init__(1/1000)
