0edb1006266cff7f7b867e00d7f218991d7aa7b7
divyareddyvanakuri <divyavanakuri48@gmail.com>
2020-02-19 11:00:06 +0530

    [Divya]refactor: Added the comment lines to the classes

--- usecase7/weights.py
+++ usecase7/weights.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 
 
+#Abstract class for weight
 class Weights(ABC):
     def __init__(self, conversion_factor):
         self.conversion_factor = conversion_factor
@@ -9,16 +10,19 @@ class Weights(ABC):
         return value*self.conversion_factor
 
 
+#Derived class from the base class
 class KgUnit(Weights):
     def __init__(self):
         super().__init__(1)
 
 
+#Derived class from the base class
 class GramUnit(Weights):
     def __init__(self):
         super().__init__(1/1000)
 
 
+#Derived class from the base class
 class TonneUnit(Weights):
     def __init__(self):
         super().__init__(1000)
