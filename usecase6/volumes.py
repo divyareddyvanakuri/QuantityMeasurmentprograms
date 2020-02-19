353213bf9cdf1b1ba40ffe4ec1938920065af3aa
divyareddyvanakuri <divyavanakuri48@gmail.com>
2020-02-19 10:54:10 +0530

    [Divya]refactor: Refactor the indentation between each code and added the comment line in the file

--- usecase6/volumes.py
+++ usecase6/volumes.py
@@ -1,6 +1,8 @@
 from abc import ABC, abstractmethod
 
 
+
+# Abstract class for Volume
 class Volumes(ABC):
     def __init__(self, conversion_factor):
         self.conversion_factor = conversion_factor
@@ -9,16 +11,18 @@ class Volumes(ABC):
         return value*self.conversion_factor
 
 
+
+#Derived class from the base class
 class LitreUnit(Volumes):
     def __init__(self):
         super().__init__(1)
 
-
+#Derived class from the base class
 class GallonUnit(Volumes):
     def __init__(self):
         super().__init__(3.78)
 
-
+#Derived class from the base class
 class MlUnit(Volumes):
     def __init__(self):
         super().__init__(1/1000)
