3fe01259131e22db150cab5a519a324f5892593f
divyareddyvanakuri <divyavanakuri48@gmail.com>
2020-02-19 10:41:27 +0530

    [Divya]refactor: Added the comment lines to the code

--- usecase4/lengths.py
+++ usecase4/lengths.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 
 
+# Abstract class for Length
 class Length(ABC):
     def __init__(self, conversion_factor):
         self.conversion_factor = conversion_factor
@@ -9,16 +10,19 @@ class Length(ABC):
         return value*self.conversion_factor
 
 
+# Derived class from the base class
 class Inchunit(Length):
     def __init__(self):
         super().__init__(1)
 
 
+# Derived class from the base class
 class FeetUnit(Length):
     def __init__(self):
         super().__init__(12)
 
 
+# Derived class from the base class
 class CentiMeterUnit(Length):
     def __init__(self):
         super().__init__(2/5)
